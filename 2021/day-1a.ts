c5000d342e050998a282a02af3e16c15f50b6bea
Garyun Young <garyun@stitched.co.uk>
2021-12-02 01:02:59 +1300

    Add getData to Day 1 Part 1 2021

--- 2021/day-1a.ts
+++ 2021/day-1a.ts
@@ -1,6 +1,18 @@
-import * as fs from 'fs'
+import getData from '../utilities'
 
-// PART ONE
+// Data
+const data = getData('day-1')
+
+const _testData: number[] = [
+  199, 200, 208, 210, 200, 207, 240, 269, 260, 263
+]
+
+const transformedData: number[] = data
+  .trim()
+  .split('\n')
+  .map((str) => Number(str))
+
+// Solution
 function countIncrements(measurements: number[]): number {
   let count: number = 0
 
@@ -13,18 +25,4 @@ function countIncrements(measurements: number[]): number {
   return count
 }
 
-const data = fs.readFileSync('inputs/day-1.txt', {
-  encoding: 'utf8',
-  flag: 'r'
-})
-
-const _testData: number[] = [
-  199, 200, 208, 210, 200, 207, 240, 269, 260, 263
-]
-
-const transformedData: number[] = data
-  .trim()
-  .split('\n')
-  .map((str) => Number(str))
-
-console.log('PART ONE:', countIncrements(transformedData))
+console.log('Answer:', countIncrements(transformedData))
