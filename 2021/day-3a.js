706a2ee40ca11bdad3d46619ad8b197e1a0aec7c
Garyun Young <garyunyoung@gmail.com>
2021-12-04 22:48:17 +1300

    Update author email, Test commit, Add Day 3 input

---
+++ 2021/day-3a.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+var utilities_1 = require("../utilities");
+//
+var data = (0, utilities_1["default"])('day-3');
+var testData = [
+    '00100',
+    '11110',
+    '10110',
+    '10111',
+    '10101',
+    '01111',
+    '00111',
+    '11100',
+    '10000',
+    '11001',
+    '00010',
+    '01010'
+];
+// Solution
+function solution(data) {
+    return 'solution';
+}
+console.log('Answer:', solution(testData));
---
+++ 2021/day-3a.ts
@@ -0,0 +1,26 @@
+import getData from '../utilities'
+
+//
+const data = getData('day-3')
+
+const testData = [
+  '00100',
+  '11110',
+  '10110',
+  '10111',
+  '10101',
+  '01111',
+  '00111',
+  '11100',
+  '10000',
+  '11001',
+  '00010',
+  '01010'
+]
+
+// Solution
+function solution(data) {
+  return 'solution'
+}
+
+console.log('Answer:', solution(testData))
