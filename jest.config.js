3b7ecaa86154103aeb13e16d9767b1cec855d3c1
Garyun Young <garyunyoung@gmail.com>
2021-12-12 12:03:47 +1300

    Remove JS files

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-module.exports = {
-    transform: { '^.+\\.ts?$': 'ts-jest' },
-    testEnvironment: 'node',
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-};
